183e4d13e4baef1e639f88696b6c8bc3c6e3d3f0
co237 <co237@uchicago.edu>
2023-05-26 11:41:16 -0400

    FECT lagged controls

--- BDS file.R
+++ BDS file.R
@@ -24,13 +24,13 @@ library(ggmap)
 library(sf)
 library(scales)
 library(plm)
-library(blscrapeR)
 library(tidycensus)
 library(censusapi)
 library (readr)
 library(tidyr)
 library(panelView)
 library(fect)
+library(readxl)
 
 #################
 ### Set paths ###
@@ -268,7 +268,7 @@ panelview(data = clean_full_bdsht_analysis_dataset, formula = estabs_entry_rate_
 clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% mutate(share_BA = BA/Pop_25_up, share_HS = HS/Pop_25_up)
 ## Industry Shares
 ### Convert job totals to numeric
-clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% mutate_at(c('emp_11','emp_21', 'emp_22', 'emp_23', 'emp_31-33','emp_42','emp_44-45', 'emp_48-49', 'emp_51', 'emp_52','emp_53','emp_54','emp_55','emp_56','emp_61','emp_62','emp_71','emp_72','emp_81'), as.numeric)
+clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% mutate_at(c('emp_11','emp_21', 'emp_22', 'emp_23', 'emp_31-33','emp_42','emp_44-45', 'emp_48-49', 'emp_51', 'emp_52','emp_53','emp_54','emp_55','emp_56','emp_61','emp_62','emp_71','emp_72','emp_81','emp_HT', 'emp_nonHT'), as.numeric)
 
 clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% mutate(total_employment = rowSums(across(c('emp_11','emp_21', 'emp_22', 'emp_23', 'emp_31-33','emp_42','emp_44-45', 'emp_48-49', 'emp_51', 'emp_52','emp_53','emp_54','emp_55','emp_56','emp_61','emp_62','emp_71','emp_72','emp_81')), na.rm = TRUE))
 
@@ -290,7 +290,34 @@ clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>%
          share_emp_61 = emp_61/total_employment,
          share_emp_62 = emp_62/total_employment,
          share_emp_71 = emp_71/total_employment,
-         share_emp_72 = emp_72/total_employment)
+         share_emp_72 = emp_72/total_employment,
+         share_emp_81 = emp_81/total_employment)
+
+
+# Create the lagged versions of the employment variables
+df_pseries <- pdata.frame(clean_full_bdsht_analysis_dataset, index = c("MSA", "year"))
+
+df_pseries$share_emp_11_lag <- lag(df_pseries$share_emp_11)
+df_pseries$share_emp_21_lag <- lag(df_pseries$share_emp_21)
+df_pseries$share_emp_22_lag <- lag(df_pseries$share_emp_22)
+df_pseries$share_emp_23_lag <- lag(df_pseries$share_emp_23)
+df_pseries$share_emp_31_33_lag <- lag(df_pseries$share_emp_31_33)
+df_pseries$share_emp_42_lag <- lag(df_pseries$share_emp_42)
+df_pseries$share_emp_44_45_lag <- lag(df_pseries$share_emp_44_45)
+df_pseries$share_emp_48_49_lag <- lag(df_pseries$share_emp_48_49)
+df_pseries$share_emp_51_lag <- lag(df_pseries$share_emp_51)
+df_pseries$share_emp_52_lag <- lag(df_pseries$share_emp_52)
+df_pseries$share_emp_53_lag <- lag(df_pseries$share_emp_53)
+df_pseries$share_emp_54_lag <- lag(df_pseries$share_emp_54)
+df_pseries$share_emp_55_lag <- lag(df_pseries$share_emp_55)
+df_pseries$share_emp_56_lag <- lag(df_pseries$share_emp_56)
+df_pseries$share_emp_61_lag <- lag(df_pseries$share_emp_61)
+df_pseries$share_emp_62_lag <- lag(df_pseries$share_emp_62)
+df_pseries$share_emp_71_lag <- lag(df_pseries$share_emp_71)
+df_pseries$share_emp_72_lag <- lag(df_pseries$share_emp_72)
+df_pseries$share_emp_81_lag <- lag(df_pseries$share_emp_81)
+
+clean_full_bdsht_analysis_dataset <- as.data.frame(df_pseries)
 
 
 
@@ -315,14 +342,246 @@ bds_national_ht_sector %>% ggplot(aes(x = year, y = estabs_entry_rate, color = h
 ###         FECT         ###
 ############################
 
+# Dependent Variable list
+
+
 
 # Drop if missing treated variable
 clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% drop_na(treated)
 # Convert startup rate to numeric
 clean_full_bdsht_analysis_dataset$estabs_entry_rate_HT <- as.numeric(clean_full_bdsht_analysis_dataset$estabs_entry_rate_HT)
+clean_full_bdsht_analysis_dataset <- clean_full_bdsht_analysis_dataset %>% mutate_at(c('treated','share_emp_11_lag','share_emp_21_lag', 'share_emp_22_lag', 'share_emp_23_lag', 'share_emp_31_33_lag','share_emp_42_lag','share_emp_44_45_lag', 'share_emp_48_49_lag', 'share_emp_51_lag', 'share_emp_52_lag','share_emp_53_lag','share_emp_54_lag','share_emp_55_lag','share_emp_56_lag','share_emp_61_lag','share_emp_62_lag','share_emp_71','share_emp_72_lag','share_emp_81_lag','estabs_entry_rate_HT'), as.numeric)
+
+
 # Basic FEct model
-out.fect <- fect(estabs_entry_rate_HT ~ treated + share_BA + share_HS + share_emp_11 + share_emp_21 + share_emp_22 + share_emp_23 + share_emp_31_33 + share_emp_42 + share_emp_44_45 + share_emp_48_49 + share_emp_51 + share_emp_52 + share_emp_53 + share_emp_54 + share_emp_55 + share_emp_56 + share_emp_61 + share_emp_62 + share_emp_71 + share_emp_72, data = clean_full_bdsht_analysis_dataset, index = c("MSA","year"), 
+out.fect <- fect(estabs_entry_rate_HT ~ treated + share_emp_11_lag + share_emp_21_lag + share_emp_22_lag + share_emp_23_lag + share_emp_31_33_lag + share_emp_42_lag + share_emp_44_45_lag + share_emp_48_49_lag + share_emp_51_lag + share_emp_52_lag + share_emp_53_lag + share_emp_54_lag + share_emp_55_lag + share_emp_56_lag + share_emp_61_lag + share_emp_62_lag + share_emp_71_lag + share_emp_72_lag + share_emp_81_lag, data = clean_full_bdsht_analysis_dataset, index = c("MSA","year"), 
                  method = "fe", force = "two-way")
+
+
 # Plot basic FEct model
 plot(out.fect, main = "Estimated ATT (FEct)", ylab = "Effect of D on Y", 
      cex.main = 0.8, cex.lab = 0.8, cex.axis = 0.8)
+
+
+############################
+###         FECT         ###
+############################
+using <- clean_full_bdsht_analysis_dataset
+
+using <- using %>% mutate(hightechshare = (emp_HT/(emp_HT + emp_nonHT))*100,
+                          share_emp_51 = share_emp_51*100)
+using <- using %>% mutate_at(c('treated','hightechshare','estabs_entry_rate_HT', 'estabs_entry_rate_nonHT', 'job_creation_rate_HT', 'job_creation_rate_nonHT','reallocation_rate_HT','reallocation_rate_nonHT'), as.numeric)
+
+dependent_list <- c("hightechshare", 
+                    "estabs_entry_rate_HT",
+                    "estabs_entry_rate_nonHT",
+                    "job_creation_rate_HT",
+                    "job_creation_rate_nonHT", 
+                    "reallocation_rate_HT",
+                    "reallocation_rate_nonHT")
+
+dependent_title <- c("Share of Employment in High Tech",
+                     "High Tech Estab. Entry Rate",
+                     "Non-High Tech Estab. Entry Rate", 
+                     "High Tech Job Creation Rate",
+                     "Non-High Tech Job Creation Rate", 
+                     "High Tech Reallocation Rate",
+                     "Non-High Tech Reallocation Rate")
+data_list <- list()
+fect_list <- list()
+
+for(j in seq_along(dependent_list)){
+  data_list[[j]] <- using
+  names(data_list[[j]])[which(colnames(data_list[[j]])==dependent_list[[j]])] <- "dependent"
+  
+}
+
+sapply(using, function(x) sum(is.na(x)))
+
+for(i in seq_along(data_list)){
+  
+  fect_list[[i]] <- fect(data = data_list[[i]],
+                         formula = dependent ~ treated +  share_emp_11_lag + share_emp_21_lag + share_emp_22_lag + share_emp_23_lag + share_emp_31_33_lag + share_emp_42_lag + share_emp_44_45_lag + share_emp_48_49_lag + share_emp_51_lag + share_emp_52_lag + share_emp_53_lag + share_emp_54_lag + share_emp_55_lag + share_emp_56_lag + share_emp_61_lag + share_emp_62_lag + share_emp_71_lag + share_emp_72 + share_emp_81_lag,
+                         force = "two-way",
+                         index = c("MSA","year"),
+                         method = "both",
+                         # method = "fe",
+                         # method = "ife",
+                         # method = "mc",
+                         nlambda = 5,
+                         vartype = "bootstrap",
+                         nboots = 1000,
+                         parallel = TRUE,
+                         cores = 8,
+                         # force = "time",
+                         min.T0 = 3,
+                         se = TRUE,
+                         na.rm = FALSE)
+}
+
+for(i in seq_along(data_list)){
+  
+  fect_list[[i]] <- fect(data = data_list[[i]],
+                         formula = dependent ~ treated,
+                         force = "two-way",
+                         index = c("MSA","year"),
+                         method = "both",
+                         # method = "fe",
+                         # method = "ife",
+                         # method = "mc",
+                         nlambda = 5,
+                         vartype = "bootstrap",
+                         nboots = 1000,
+                         parallel = TRUE,
+                         cores = 8,
+                         # force = "time",
+                         min.T0 = 3,
+                         se = TRUE,
+                         na.rm = FALSE)
+}
+
+for(i in seq_along(fect_list)){
+  print("***********************************")
+  print(i)
+  print(dependent_title[[i]])
+  print("***********************************")
+  print(fect_list[[i]])
+  print("***********************************")
+  print("***********************************")
+}
+
+#############################################
+
+###    Counter Factual Plot               ###
+
+#############################################
+
+Outcome_paths <- list()
+counterfactual_plots <- list()
+
+for (i in seq_along(fect_list)) {
+  ###############################
+  
+  # Define who is and is not treated via the FEct
+  IDs_treatmentstatus <- as.data.frame(cbind(fect_list[[i]][["id"]],fect_list[[i]][["unit.type"]]))
+  IDs_treated <- IDs_treatmentstatus %>% filter(V2!=1)
+  IDs_treated_list <- as.vector(IDs_treated$V1)
+  
+  IDs_control <- IDs_treatmentstatus %>% filter(V2==1)
+  IDs_control_list <- as.vector(IDs_control$V1)
+  
+  idx <- match(IDs_treated_list, names(as.data.frame(fect_list[[i]][["eff"]])))
+  idx_control <- match(IDs_control_list, names(as.data.frame(fect_list[[i]][["eff"]])))
+  
+  ###########################
+  # capture the average predicted path for each age from FEct 
+  eff <- as.data.frame(fect_list[[i]][["eff"]])[,idx] 
+  Y.ct <- as.data.frame(fect_list[[i]][["Y.ct"]])[,idx] 
+  
+  eff <- as.data.frame(rowMeans(eff, na.rm = TRUE))
+  Y.ct <- as.data.frame(rowMeans(Y.ct, na.rm = TRUE))
+  
+  effect_df <- cbind(eff,Y.ct,fect_list[[i]][["rawtime"]])
+  names(effect_df) <- c("Effect","Predicted","Year")
+  
+  Outcome_paths[[i]] <- effect_df
+  #############################
+  # Calculate the observed average path for treated and control units
+  
+  treat <- data_list[[i]] %>% filter(MSA %in% IDs_treated_list) %>% group_by(year) %>% summarise(Treated = mean(dependent,na.rm=TRUE)) %>% mutate(Year = year)
+  control <- data_list[[i]] %>% filter(MSA %in% IDs_control_list) %>% group_by(year) %>% summarise(Control = mean(dependent,na.rm=TRUE)) %>% mutate(Year = year)
+  
+  Outcome_paths[[i]] <- left_join(Outcome_paths[[i]],treat)
+  Outcome_paths[[i]] <- left_join(Outcome_paths[[i]],control)
+  
+  counterfactual_plots[[i]] <- Outcome_paths[[i]] %>% 
+    ggplot(aes(x = Year)) +
+    geom_vline(xintercept = 2008) +
+    geom_line(aes(y = Predicted,color = 'Predicted')) + 
+    geom_line(aes(y = Treated,color = 'Treated')) + 
+    geom_point(aes(y = Predicted,color = 'Predicted')) + 
+    geom_point(aes(y = Treated,color = 'Treated')) + 
+    ggtitle(dependent_title[[i]]) +
+    ylab("Predicted or Observed") +
+    theme(plot.title = element_text(size=20),
+          axis.text.x = element_text(size = 15),
+          axis.text.y = element_text(size = 15),
+          axis.title.y = element_text(size = 18),
+          axis.title.x = element_text(size = 18),
+          strip.text.x = element_text(size = 20),
+          strip.text.y = element_text(size = 15),
+          strip.background = element_rect(fill = "white"),
+          panel.background = element_rect(fill = "white"),
+          panel.grid = element_line(colour = "grey"),
+          panel.spacing = unit(2, "lines"),
+          axis.line = element_line(colour = "black"),
+          legend.position = "bottom",
+          legend.title = element_blank(),
+          legend.key = element_rect(fill="white"),
+          legend.key.width = unit(2,"cm"),
+          legend.text = element_text(size = 18),
+          legend.background = element_rect(fill=NA),
+          plot.caption = element_text(hjust = 0.5,size = 15))    
+  
+}
+
+for(i in seq_along(counterfactual_plots)){
+  plot(counterfactual_plots[[i]])
+}
+
+
+ATT <- list()
+se <- list()
+
+for (i in seq_along(fect_list)) {
+  ATT[[i]] <- fect_list[[i]]$att.avg
+  
+  weighted_boots <- fect_list[[i]][["att.avg.boot"]]
+  weighted_boots <- as.data.frame(t(weighted_boots))
+  
+  se[[i]] <- sd(weighted_boots$V1)
+}
+
+results_fect <- as.data.frame(cbind(dependent_list,
+                                    dependent_title,
+                                    do.call(rbind,ATT),
+                                    do.call(rbind,se)))
+results_fect <- results_fect %>% rename("ATT" = "V3",
+                                        "SE" = "V4") %>%
+  mutate(ATT = as.numeric(ATT),
+         SE = as.numeric(SE))
+
+
+results_fect <- results_fect %>% mutate(Significant = if_else((ATT - SE*1.95)>0,"Positive, Sig. (95%)",
+                                                              if_else((ATT + SE*1.95)<0, "Negative, Sig. (95%)","Insignificant")))
+
+group.colors <- c("Positive, Sig. (95%)" = "darkgreen", "Negative, Sig. (95%)" = "darkred","Insignificant" ="grey50")
+
+P <- results_fect %>% 
+  ggplot(aes(x = dependent_title,
+             y = ATT,
+             color = Significant)) + 
+  geom_hline(yintercept = 0) +
+  geom_point(size = 4) + 
+  geom_errorbar(aes(ymin = ATT - 1.95*SE, ymax = ATT + 1.95*SE)) + 
+  scale_color_manual(values=group.colors)+ 
+  theme(plot.title = element_text(size=20),
+        axis.text.y = element_text(size = 25),
+        axis.text.x = element_text(size = 15, angle = 90),
+        axis.title.y = element_text(size = 30),
+        axis.title.x = element_text(size = 30),
+        strip.text.x = element_text(size = 30),
+        strip.text.y = element_text(size = 30),
+        strip.background = element_rect(fill = "white"),
+        strip.placement = "outside",
+        panel.background = element_rect(fill = "white"),
+        panel.grid = element_line(colour = "grey"),
+        panel.spacing = unit(2, "lines"),
+        axis.line = element_line(colour = "black"),
+        legend.position = "bottom",
+        legend.title = element_text(size = 20),
+        legend.key = element_rect(fill="white"),
+        legend.text = element_text(size = 25),
+        legend.background = element_rect(fill=NA)) +
+  guides(col = guide_legend(title = "")) 
+P
